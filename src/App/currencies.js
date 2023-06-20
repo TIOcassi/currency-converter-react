392190e5eab8bb88f4c97ca49463256e6119dcc0
Krystian Zajac <tiocassi1@wp.pl>
2023-06-20 10:49:48 +0200

    Initialize project using Create React App

---
+++ src/App/currencies.js
@@ -0,0 +1,12 @@
+export const currencies = [
+            {
+                        short: "USD",
+                        name: "USD",
+                        rate: 4.38,
+            },
+            {
+                        short: "EUR",
+                        name: "Euro",
+                        rate: 4.65,
+            },
+];
---
+++ src/App/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './index.css';
+import { currencies } from '.currencies';
+
+function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+      setResult ({
+        sourceAmount: +amount,
+        targetAmount: amount / rate,
+        currency,
+      });
+  }
+
+return (
+  <div className="app">
+    <Form
+    result={result}
+    calculateResult={calculateResult}
+    />
+  </div>
+);
+}
+
+export default App;
