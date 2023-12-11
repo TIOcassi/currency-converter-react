6d43e3967358086170598369b97e3c666d3d8aa7
Krystian Zajac <tiocassi1@wp.pl>
2023-12-11 16:44:30 +0100

    changed position of 'return'

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -11,54 +11,53 @@ export const Form = ({ calculateResult, result }) => {
             event.preventDefault();
             calculateResult(currency, amount);
       }
+      return (
+            <form className="form" onSubmit={onSubmit}>
+                  <h1 className="form__header">
+                        Przelicznik walut
+                  </h1>
+                  <p>
+                        <label>
+                              <span className="form__labelText">
+                                    Kwota w zł*:
+                              </span>
+                              <input
+                                    value={amount}
+                                    onChange={({ target }) => setAmount(target.value)}
+                                    placeholder="wpisz kwotę w zł"
+                                    className="form__field"
+                                    type="number"
+                                    required
+                                    step="0.01"
+                              />
+                        </label>
+                  </p>
+                  <p>
+                        <label>
+                              <span className="form__labelText">
+                                    Waluta:
+                              </span>
+                              <select
+                                    className="form__field"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
+                              >
+                                    {currencies.map((currency => (
+                                          <option
+                                                key={currency.short}
+                                                value={currency.short}
+                                          >
+                                                {currency.name}
+                                          </option>
+                                    )))}
+                              </select>
+                        </label>
+                  </p>
+                  <p>
+                        <button className="form__button">Przelicz!</button>
+                  </p>
+      
+                  <Result result={result} />
+            </form>
+      );
 }
-
-return (
-      <form className="form" onSubmit={onSubmit}>
-            <h1 className="form__header">
-                  Przelicznik walut
-            </h1>
-            <p>
-                  <label>
-                        <span className="form__labelText">
-                              Kwota w zł*:
-                        </span>
-                        <input
-                              value={amount}
-                              onChange={({ target }) => setAmount(target.value)}
-                              placeholder="wpisz kwotę w zł"
-                              className="form__field"
-                              type="number"
-                              required
-                              step="0.01"
-                        />
-                  </label>
-            </p>
-            <p>
-                  <label>
-                        <span className="form__labelText">
-                              Waluta:
-                        </span>
-                        <select
-                              className="form__field"
-                              value={currency}
-                              onChange={({ target }) => setCurrency(target.value)}
-                        >
-                              {currencies.map((currency => (
-                                    <option
-                                          key={currency.short}
-                                          value={currency.short}
-                                    >
-                                          {currency.name}
-                                    </option>
-                              )))}
-                        </select>
-                  </label>
-            </p>
-            <p>
-                  <button className="form__button">Przelicz!</button>
-            </p>
-
-            <Result result={result} />
-      </form>
-);
