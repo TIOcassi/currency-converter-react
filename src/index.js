a841f0b192d1500ea9d205374fdbeb444dfc5224
Krystian Zajac <tiocassi1@wp.pl>
2024-05-01 16:11:34 +0200

    App

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle></GlobalStyle>
+      <GlobalStyle />
+        <App />
     </ThemeProvider>
-    <App />
   </React.StrictMode>
 );
 
