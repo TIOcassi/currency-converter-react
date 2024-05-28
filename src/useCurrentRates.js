3eba05e71f1241a72ded46fbfbdc1c43dfcc0cc2
Krystian Zajac <tiocassi1@wp.pl>
2024-05-28 16:07:25 +0200

    axios, await

--- src/useCurrentRates.js
+++ src/useCurrentRates.js
@@ -3,17 +3,28 @@ import { useEffect, useState } from "react";
 
 export const useCurrentRates = () => {
     const [rates, setRates] = useState({ state: "loading" });
+
     useEffect(() => {
         const rates = async () => {
             try {
-                const data = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_F5cZh8xZQbbbuJXMb96MMbM2pjSN3TLER0SnHHDQ&base_currency=PLN");
+                const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_T7igunZa2FeNXCeW5aRB0dR&base_currency=PLN");
                 setRates(data.data.data);
+
+                const {meta, data} = await response.data;
+
+                setRates({
+                    state: "success",
+                    rates: data,
+                    date: meta,
+                });
+
             }
             catch {
                 setRates({ state: "error" });
             }
         };
-        rates();
+        setTimeout(rates, 1000);
     }, []);
+    
     return rates;
 };
